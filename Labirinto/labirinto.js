6f787cc89aa4816f77f11faf913f8f7d99051149
Bruno Emer <emerbruno@gmail.com>
2015-08-28 18:39:30 -0300

    Correcao validacao de movimentos do labirinto e pdf do trabalho

--- Labirinto/labirinto.js
+++ Labirinto/labirinto.js
@@ -30,29 +30,38 @@ function Labirinto() {
 	this.direita = '10';
 	this.esquerda = '11';
 	
+	/*
+		Retorna:
+		0 para valido
+		1 quando atravessar parede
+		2 quando sair do labirinto
+	*/
 	this.validaMovimento = function(posicao, movimento) {
 		var valido = 0;
-		
-		console.info(posicao+" - "+movimento+" - "+parseInt(posicao / this.tamanho));
-		
-		if(parseInt(posicao / this.tamanho) == 0) { //linha 1
-			if(movimento == this.cima) {
-				valido = 2;
-			}
-		} else if(parseInt(posicao / this.tamanho) == this.tamanho-1) { //ultima linha
-			if(movimento == this.baixo) {
-				valido = 2;
-			}
-		} else if(posicao % this.tamanho == 0) { //coluna esquerda
+
+		if(posicao % this.tamanho == 0) { //coluna esquerda
+			console.info(3);
 			if(movimento == this.esquerda) {
 				valido = 2;
 			}
 		} else if(posicao % this.tamanho == this.tamanho-1) { //coluna direita
+			console.info(4);
 			if(movimento == this.direita) {
+				valido = 2;
+			}
+		} else if(parseInt(posicao / this.tamanho) == 0) { //linha 1
+			console.info(1);
+			if(movimento == this.cima) {
+				valido = 2;
+			}
+		} else if(parseInt(posicao / this.tamanho) == this.tamanho-1) { //ultima linha
+			console.info(2);
+			if(movimento == this.baixo) {
 				valido = 2;
 			}
 		}
 		if(valido > 0) {
+			//console.info(posicao+" - "+movimento+" - "+valido);
 			return valido;
 		}
 		
@@ -61,7 +70,17 @@ function Labirinto() {
 			valido = 1;
 		}
 		
+		//console.info(posicao+" - "+movimento+" - "+valido);
+		
 		return valido;
 		
 	};
 }
+
+$(document).ready(function() {
+	var lab = new Labirinto();
+	//lab.validaMovimento(10, '10');
+	//lab.validaMovimento(4, '11');
+	//lab.validaMovimento(2, '00');
+	//lab.validaMovimento(21, '01');
+});
